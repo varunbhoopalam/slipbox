ab1ce2d7c407f0c1cbf9361fb1d46dd88842f5e3
varunbhoopalam <bhoopalam.varun@gmail.com>
2020-05-15 19:16:01 -0500

    can contact api

--- src/Main.elm
+++ src/Main.elm
@@ -8,57 +8,73 @@ module Main exposing (..)
 
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
-
-
+import Html exposing (Html, text, pre)
+import Http
 
 -- MAIN
 
-
 main =
-  Browser.sandbox { init = init, update = update, view = view }
-
-
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
 -- MODEL
 
+type Model 
+  = Failure
+  | Loading
+  | Success String
 
-type alias Model = Int
 
+init : () -> (Model, Cmd Msg)
+init _ =
+  (
+    Loading
+    , Http.get
+    {
+      expect = Http.expectString GotText,
+      url = "http://localhost:5000"
+    }
+  )
 
-init : Model
-init =
-  0
 
 
 
 -- UPDATE
-
-
 type Msg
-  = Increment
-  | Decrement
+  = GotText (Result Http.Error String)
 
-
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    GotText result ->
+      case result of
+        Ok fullText ->
+          (Success fullText, Cmd.none)
 
-    Decrement ->
-      model - 1
+        Err _ ->
+          (Failure, Cmd.none)
+
+-- SUBSCRIPTIONS
 
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 -- VIEW
 
-
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+  case model of
+    Failure ->
+      text "I was unable to json."
+
+    Loading ->
+      text "Loading..."
+
+    Success fullText ->
+      pre [] [ text fullText ]
