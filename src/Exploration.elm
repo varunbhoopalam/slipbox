ce464585ca453e795fd0abaa06292e43414f6012
varunbhoopalam <bhoopalam.varun@gmail.com>
2020-08-01 14:17:49 -0500

    Working Question Filter

--- src/Exploration.elm
+++ src/Exploration.elm
@@ -1,8 +1,9 @@
 module Exploration exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, div, input, text)
-import Html.Attributes exposing (id)
+import Html exposing (Html, Attribute, div, input, text, button)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
 import Force exposing (entity)
 
 
@@ -17,12 +18,20 @@ main =
 -- MODEL
 
 
-type Model = Model Notes
+type Model = Model Notes QuestionQuery
 
 
 init : Model
 init =
-  Model (initializeNotes [ Note 1 "Question 0" "" "index", Note 2 "Question 1" "" "index", Note 3 "Note 0" "" "note"])
+  Model (initializeNotes initNoteData) (Shown "")
+
+initNoteData : List Note
+initNoteData = 
+  [ Note 1 "What is the Elm langauge?" "" "index"
+  , Note 2 "Why does some food taste better than others?" "" "index"
+  , Note 3 "Note 0" "" "note"]
+
+
 
 -- NOTES
 type Notes = Notes (List PositionNote)
@@ -83,15 +92,38 @@ getIndexQuestions n =
 toViewNote: PositionNote -> ViewNote
 toViewNote pn =
   ViewNote pn.id pn.content pn.source
+
+-- QuestionFilter
+type QuestionQuery = 
+  Shown String |
+  Hidden String
+
+reverseQuestionQueryState: QuestionQuery -> QuestionQuery
+reverseQuestionQueryState q =
+  case q of
+    Shown s -> Hidden s
+    Hidden s -> Shown s
+
+updateQuestionQuery: String -> QuestionQuery -> QuestionQuery
+updateQuestionQuery s q =
+  case q of
+    Shown _ -> Shown s
+    Hidden _ -> Hidden s
+
 -- UPDATE
 
-type Msg
-  = Change String
+type Msg = 
+  Change String |
+  ShowQuestionList
 
 
 update : Msg -> Model -> Model
-update _ model =
-  model
+update msg model =
+  case model of
+    Model n q ->
+      case msg of 
+        ShowQuestionList -> Model n (reverseQuestionQueryState q)
+        Change s -> Model n (updateQuestionQuery s q)
 
 -- VIEW
 
@@ -99,18 +131,37 @@ update _ model =
 view : Model -> Html Msg
 view m =
   div []
-    [ div [id "Question-Filter"] (questionFilterView m)
+    [ div [id "Questions"] [questionView m]
     , div [id "Control-Ship"] [ text "Control Ship" ]
     , div [id "History-Queue"] [ text "History Queue"]
     ]
+  
+questionView : Model -> Html Msg
+questionView m =
+  case m of 
+    Model n q ->
+      case q of
+        Shown query -> div [id "Questions"] 
+          [ questionFilter query
+          , div [id "Question List"] (questionList n query)
+          , questionButton
+          ]
+        Hidden _ -> questionButton
 
-questionFilterView: Model -> List (Html Msg)
-questionFilterView m =
-  case m of
-    Model n -> n
-      |> getIndexQuestions
-      >> List.map divFromViewNote 
+questionButton: Html Msg
+questionButton = 
+  button [ id "Show Question Button", onClick ShowQuestionList ] [ text "Q" ]
 
+questionFilter: String -> Html Msg 
+questionFilter s = 
+  input [placeholder "Question Filter", value s, onInput Change] []
+
+questionList: Notes -> String -> List (Html Msg)
+questionList n query =
+  n 
+    |> getIndexQuestions 
+    |> List.filter (\note -> String.contains query note.content)
+    |> List.map divFromViewNote 
 
 divFromViewNote: ViewNote -> Html Msg
 divFromViewNote v =
