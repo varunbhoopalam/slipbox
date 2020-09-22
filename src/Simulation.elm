82d694ba0adea295b105eb01fa8c2869217803b4
varunbhoopalam <bhoopalam.varun@gmail.com>
2020-09-21 22:21:51 -0500

    update simulation

--- src/Simulation.elm
+++ src/Simulation.elm
@@ -14,19 +14,19 @@ type alias SimulationRecord =
 init: Int -> SimulationRecord
 init id =
   let
-    entity = Force.entity ((id + 1)*10) 1
+    entity = Force.entity id 1
   in
     SimulationRecord id entity.x entity.y entity.vx entity.vy
 
 simulate: (List SimulationRecord) -> (List (Int, Int)) -> (List SimulationRecord)
 simulate records links =
-  List.map toSimulationRecord (Force.computeSimulation (Force.iterations 100 (stateBuilder records links)) records)
+  List.map toSimulationRecord (Force.computeSimulation (Force.iterations 300 (stateBuilder records links)) records)
   -- List.map toSimulationRecord (Tuple.second (Force.tick (stateBuilder records links) records))
 
 stateBuilder: (List SimulationRecord) -> (List (Int, Int)) -> Force.State Int
 stateBuilder records links =
-  Force.simulation 
-        [ Force.manyBodyStrength -10 (List.map (\n -> n.id) records)
+  Force.simulation
+        [ Force.manyBodyStrength -15 (List.map (\n -> n.id) records)
         , Force.links links
         , Force.center 0 0
         ]
