5d5529cf0943002f6beb18b9d9aed036f01c9972
varunbhoopalam <bhoopalam.varun@gmail.com>
2021-04-12 22:20:42 -0500

    Implement Explore module

--- src/Explore.elm
+++ src/Explore.elm
@@ -20,6 +20,17 @@ type alias HoveredNote = Maybe Note.Note
 -- Note: This graph will not have anything selected, all will always be regular notes.
 -- If someone clicks a note it will simply open that discussion in discovery mode
 
+type ExploreView
+  = ErrorStateNoDiscussionsView
+  | ForceDirectedGraphView Graph.Graph HoveredNote
+
+view : Explore -> ExploreView
+view explore =
+  case explore of
+    ErrorStateNoDiscussions -> ErrorStateNoDiscussionsView
+    ForceDirectedGraph graph hoveredNote -> ForceDirectedGraphView graph hoveredNote
+
+
 init : Slipbox.Slipbox -> Explore
 init slipbox =
   let discussions = Slipbox.getDiscussions Nothing slipbox
@@ -30,3 +41,17 @@ init slipbox =
     ForceDirectedGraph
       ( Graph.simulatePositions <| Slipbox.getAllDiscussionsAndLinksBetweenDiscussions slipbox )
       Nothing
+
+hover : Note.Note -> Explore -> Explore
+hover note edit =
+  case edit of
+    ForceDirectedGraph graph _ ->
+      ForceDirectedGraph graph <| Just note
+    _ -> edit
+
+stopHover : Explore -> Explore
+stopHover edit =
+  case edit of
+    ForceDirectedGraph graph _ ->
+      ForceDirectedGraph graph Nothing
+    _ -> edit
